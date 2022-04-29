784b015542d141d3ee23fd0c74d369cf6ab133b6
Maurizio <maurizioalfaro@gmail.com>
2022-04-29 12:14:54 -0500

    backup to change machine

--- models/Ticket.js
+++ models/Ticket.js
@@ -10,6 +10,14 @@ const TicketSchema = Schema({
         type: String,
         required: true
     },
+    type: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
     date: {
         type: String,
         required: true
